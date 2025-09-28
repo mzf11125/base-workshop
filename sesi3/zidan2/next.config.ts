6007069860b3ee78a6f8aff151eaf99102c6db2b
M Zidan Fatonie <77999453+mzf11125@users.noreply.github.com>
2025-09-28 13:22:12 +0700

    update

--- sesi3/zidan2/next.config.ts
+++ sesi3/zidan2/next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/about',
-        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/01998eaf-3909-9238-da15-b05e1e75cab3',
-        permanent: true,
-      },
-    ]
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
   },
 };
 
